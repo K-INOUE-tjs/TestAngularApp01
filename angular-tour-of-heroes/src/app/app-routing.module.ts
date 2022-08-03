1eb71b6af6c3c6a46eb39887a6347326360f8575
Kenji Inoue <mos.con5000oda9@gmail.com>
2022-08-03 17:58:25 +0900

    Angular Tutorial 2022/8/3

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+//アプリケーションにルーティング機能を持たせることができる
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';//RouterModule と Routes をインポート
+import { HeroesComponent } from './heroes/heroes.component'; //ルートを設定することでルーターに向かう場所を教える
+import { DashboardComponent } from './dashboard/dashboard.component';//app-routing.module.tsでDashboardComponentをインポート
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },//アプリケーションをダッシュボードへ自動的に置き換える為、routes配列に追加...
+  { path: 'dashboard', component: DashboardComponent },//routes配列に、 DashboardComponentへのパスにマッチするルートを追加
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  //▼RouterModule を AppRoutingModule の imports 配列に追加することで、RouterModule.forRoot() を呼び出し一発で routes 設定できる...
+  imports: [ RouterModule.forRoot(routes) ],
+
+  //▼AppRoutingModule は RouterModule をエクスポートし、アプリケーション全体で利用できるようにする。
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
